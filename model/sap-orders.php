4fa0a31611af2839b05861f5801e9fbf7ea67e14
Yevhenii <zenia9012@gmail.com>
2020-05-12 12:17:19 +0300

    added column

--- model/sap-orders.php
+++ model/sap-orders.php
@@ -99,6 +99,7 @@ class SapOrders {
             $stmt->bindParam(13, $item['Comments']);
             $stmt->bindParam(14, $item['LastReportUpdate']);
             $stmt->bindParam(15, $item['SAP source']);
+            $stmt->bindParam(16, $item['InvntSttus']);
             $stmt->execute();
         }
     }
--- src/queries.php
+++ src/queries.php
@@ -2,8 +2,8 @@
 
 return [
     "insertToSapDB" => "INSERT INTO sap_orders 
-                (DocNum,NumatCard,CardCode,CardName,DocDate,DocTime,Aging,SlpName,WhsName,GroupName,Printed,Address,Comments,LastReportUpdate,SapSource)
-                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (DocNum,NumatCard,CardCode,CardName,DocDate,DocTime,Aging,SlpName,WhsName,GroupName,Printed,Address,Comments,LastReportUpdate,SapSource,InvntSttus)
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
     "insertToWebsiteDB" => "INSERT INTO website_orders 
                 (order_id,customer,status,total,sap_status,date_added,website,country,address,payment_method,last_updated)
                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)",
