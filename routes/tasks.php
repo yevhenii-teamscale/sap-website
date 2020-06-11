0f8392e99e8ddf96126e0a814661419669c9a3d9
Yevhenii <zenia9012@gmail.com>
2020-06-11 13:23:59 +0300

    update link

--- routes/tasks.php
+++ routes/tasks.php
@@ -8,9 +8,9 @@ use Slim\Http\Response;
 $app->get('/update-all', function (Request $request, Response $response, $args) {
 
     echo 'start updating' . PHP_EOL;
-    echo 'updating sap-orders' . PHP_EOL;
+    echo 'updating sap orders' . PHP_EOL;
 
-    exec('php index.php sap orders');
+    exec('php index.php sap-orders');
 
     echo 'updating web orders' . PHP_EOL;
 
